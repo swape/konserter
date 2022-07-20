d0aefbe9242425c016325061df5d461aadaea69f
Alireza Balouch <swape@swape.net>
2022-07-20 16:35:55 +0200

    listing concerts

--- src/lib/Concertlist/index.svelte
+++ src/lib/Concertlist/index.svelte
@@ -1,6 +1,53 @@
 <script lang="ts">
-import { userObj } from '../../myStore'
-console.log(userObj.uid)
+    import {userObj} from '../../myStore'
+    import {syncItems} from '../../fire.js'
+    import {ConcertObjectType} from "../../types";
+
+    let concertList: ConcertObjectType[] = []
+
+    function cleanDateToNumber(value: string): number {
+        if (!value.date) {
+            return 0
+        }
+        return parseInt(value.date.replace(/-/g, ''), 10)
+    }
+
+    function sortByDate(a, b) {
+        const cleanedA = cleanDateToNumber(a)
+        const cleanedB = cleanDateToNumber(b)
+        return cleanedA < cleanedB ? 1 : -1
+    }
+
+    syncItems(userObj.uid, (data: any) => {
+        concertList = Object.values(data).sort(sortByDate).filter((_, index) => index < 9)
+    })
+
+    function getHeader(concert: ConcertObjectType) {
+        return `${concert?.artist}${concert?.venue ? ' @ ' + concert.venue : ''}`
+    }
 </script>
 
-<div>Her kommer de siste 10 registreringene</div>
+
+{#each concertList as concert}
+    <div class="box">
+        <h2 class="header">{getHeader(concert)} </h2>
+        <div>
+            <div class="flex justify-between text-sm text-gray-400 items-center">
+                <small>{concert.date}</small>
+                {#if concert.festival}
+                    <div>{concert.festival}</div>
+                {/if}
+                {#if concert.rating}
+                    <div class="flex items-center">
+                        <span class="material-icons">star</span>
+                        <span>{concert.rating}</span>
+                    </div>
+                {/if}
+
+            </div>
+
+            <div class="text-sm text-gray-500">{concert.note}</div>
+        </div>
+    </div>
+{/each}
+
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,15 +1,16 @@
-<style>
-</style>
-
 <script lang="ts">
-import ConsertList from '$lib/Concertlist/index.svelte'
+  import ConsertList from '$lib/Concertlist/index.svelte'
 </script>
 
 <svelte:head>
-  <title>Home</title>
-  <meta name="description" content="LMS" />
+    <title>Home</title>
+    <meta name="description" content="LMS"/>
 </svelte:head>
 
-<div class="box">
-  <ConsertList />
+<div class="w-full">
+    <div class="grid grid-cols-1 w-full justify-items-center gap-5">
+        <ConsertList/>
+    </div>
 </div>
+
+
