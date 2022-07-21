2516f6893a888da196e761748e0d0097c282ac8f
Alireza Balouch <swape@swape.net>
2022-07-21 12:21:03 +0200

    adding filter search

--- src/routes/list/index.svelte
+++ src/routes/list/index.svelte
@@ -3,9 +3,14 @@
     import {syncItems} from '../../fire.js'
     import {getArtistAndVenue, sortByDate} from "$lib/Concertlist/helper"
     import {goto} from "$app/navigation"
+    import InputWithLabel from "../../lib/InputWithLabel/index.svelte";
+    import {ConcertObjectType} from "../../types";
 
+    let filterText = ''
 
     let concertList = []
+    $: filteredList = filterList(concertList, filterText)
+
 
     syncItems(userObj.uid, (data: any) => {
         concertList = Object.values(data).sort(sortByDate)
@@ -14,10 +19,31 @@
     function gotoConcert(id: string) {
         goto(`/new/${id}`)
     }
+
+    function isIn(value: string, filterValue: string) {
+        return (value.toLocaleLowerCase().search(filterValue.toLocaleLowerCase()) !== -1)
+    }
+
+    function filterList(innerConcertList: ConcertObjectType[], filterValue: string) {
+        if (filterValue) {
+            return innerConcertList.filter((concert: ConcertObjectType) => {
+                return isIn(`${concert.rating}`, filterValue) || isIn(concert.date, filterValue) || isIn(concert.artist, filterValue) || isIn(concert.venue, filterValue) || isIn(concert.festival, filterValue)
+            })
+        }
+        return innerConcertList
+    }
+
+
 </script>
-<div>
-  <ul>
-    {#each concertList as concert}
+<div class="box">
+
+  <div>
+    <InputWithLabel title="Filter" bind:value={filterText}/>
+    <div class="text-xs mt-2">Resultater: {filteredList.length}</div>
+  </div>
+
+  <ul class="p-3">
+    {#each filteredList as concert}
       <li>
         <button on:click={()=>gotoConcert(concert.id)}
                 class="concert-button">
@@ -35,7 +61,7 @@
 
 <style>
     .concert-button {
-        @apply grid grid-cols-3 bg-slate-900 m-1 p-2 text-sm w-full overflow-clip rounded border text-white;
+        @apply grid grid-cols-3 bg-slate-900 my-1 p-2 text-sm w-full overflow-clip rounded text-white;
     }
 
 </style>
