508607fa430a9157e5b4969a972a5072fdb55144
Alireza Balouch <gswape@gmail.com>
2023-02-15 21:25:53 +0100

    adding statGraph

---
+++ src/pages/stats/components/StatGraph/index.svelte
@@ -0,0 +1,38 @@
+<script lang="js">
+import { concerts } from '../../../../myStore'
+import { filterByGivenYear, getGraphData } from '../../../../helper'
+import { LinkedChart } from 'svelte-tiny-linked-charts'
+import ConsertBox from '../../../frontpage/components/ConcertBox/index.svelte'
+
+export let year = ''
+let conserts = []
+let graphData = []
+let details = ''
+let filteredConserts = []
+
+$: {
+	concerts.subscribe((data) => {
+		conserts = filterByGivenYear(data, parseInt(year, 10))
+		graphData = getGraphData(conserts)
+		filteredConserts = []
+		details = ''
+	})
+}
+
+function addDetails(value) {
+	filteredConserts = conserts.filter((consert) => consert.date === value.key)
+	details = `${value.key}: ${value.value}`
+}
+</script>
+
+{#if conserts.length > 0}
+	<div class="flex justify-between bg-white m-3 p-3 rounded-md items-center">
+		<LinkedChart data={graphData} on:hover={(event) => addDetails(event.detail)} dispatchEvents={true} />
+		<div>{details}</div>
+	</div>
+	<div class="flex flex-col m-3 rounded-md mt-6">
+		{#each filteredConserts as concert}
+			<ConsertBox concert={concert} />
+		{/each}
+	</div>
+{/if}
--- src/pages/stats/index.svelte
+++ src/pages/stats/index.svelte
@@ -1,9 +1,29 @@
 <script>
 import BestVenues from './components/BestVenues/index.svelte'
 import TotalCost from './components/TotalCost/index.svelte'
+import StatGraph from './components/StatGraph/index.svelte'
+
+const nowYear = new Date().getFullYear()
+
+const years = [...Array(5)].map((_, i) => `${nowYear - i}`)
+
+let selected = '' + nowYear
+
+function handleOnClick() {
+	console.log(selected)
+}
 </script>
 
 <main>
-	<TotalCost />
-	<BestVenues />
+	<div class="p-3">
+		<select bind:value={selected} class="button" on:change={handleOnClick}>
+			{#each years as year}
+				<option value={year}>{year}</option>
+			{/each}
+		</select>
+	</div>
+
+	<TotalCost year={selected} />
+	<BestVenues year={selected} />
+	<StatGraph year={selected} />
 </main>
