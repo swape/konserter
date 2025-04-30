1505d9a66841d9bdfa52f67e5e3b6fe873673e6c
Alireza Balouch <gswape@gmail.com>
2025-04-30 12:50:24 +0200

    removing unused functions

--- src/fire.js
+++ src/fire.js
@@ -1,6 +1,6 @@
 import {initializeApp} from 'firebase/app'
-import {getAuth, GoogleAuthProvider, onAuthStateChanged, signInWithRedirect, signOut, getRedirectResult} from 'firebase/auth'
-import {child, getDatabase, onValue, push, query, ref, remove, set, update} from 'firebase/database'
+import {getAuth, GoogleAuthProvider, onAuthStateChanged, signInWithRedirect, signOut} from 'firebase/auth'
+import {child, getDatabase, onValue, push, query, ref, update} from 'firebase/database'
 
 import {firebaseConfig} from './config.js'
 import {isAuthenticated, userObj, concerts} from './myStore.ts'
@@ -23,32 +23,6 @@ onAuthStateChanged(auth, (dUser) => {
 	}
 })
 
-export const init = (cb) => {
-	if (auth.currentUser) {
-		return cb(saveUser(auth.currentUser))
-	}
-	onAuthStateChanged(auth, (dUser) => {
-		if (dUser?.uid) {
-			cb(saveUser(dUser))
-		} else {
-			cb(null)
-		}
-	})
-
-	getRedirectResult(auth)
-		.then((result) => {
-			const user = result.user
-			if (user) {
-				cb(saveUser(user))
-			}
-		})
-		.catch(({code, message}) => {
-			if (code) {
-				console.error({code, message})
-			}
-		})
-}
-
 export const fireSignOut = () => signOut(auth)
 
 export const fireSignIn = () => signInWithRedirect(auth, provider)
@@ -68,20 +42,6 @@ export const saveUser = (dUser) => {
 	}
 }
 
-export const getData = (path) => {
-	return new Promise((resolve, reject) => {
-		const starCountRef = ref(database, path)
-		onValue(starCountRef, (snapshot) => {
-			const data = snapshot.val()
-			if (data) {
-				resolve(data)
-			} else {
-				reject(new Error(`Could not find ${path}`))
-			}
-		})
-	})
-}
-
 export const addEntry = (name, data) => {
 	return new Promise((resolve) => {
 		const newPostKey = push(child(ref(database), name)).key
@@ -96,14 +56,6 @@ export const addEntry = (name, data) => {
 
 export const updateEntry = (name, data) => {
 	return update(ref(database, name), data)
-}
-
-export const setEntry = (name, data) => {
-	return set(ref(database, name), data)
-}
-
-export const deleteEntry = (name) => {
-	return remove(ref(database, name))
 }
 
 export const syncItems = (path, cb) => {
