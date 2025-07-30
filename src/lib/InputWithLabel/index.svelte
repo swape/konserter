b51e4faa31f321e166f36f30dafce655b8221891
Alireza Balouch <gswape@gmail.com>
2025-07-30 11:04:30 +0200

    refactor: add onkeyup event handler to input fields for better accessibility

--- src/lib/InputWithLabel/index.svelte
+++ src/lib/InputWithLabel/index.svelte
@@ -1,17 +1,17 @@
 <script>
-let {value = null, title = '', type = 'text', postfix = null, onchange = () => {}} = $props()
+let {value = null, title = '', type = 'text', postfix = null, onchange = () => {}, onkeyup = () => {}} = $props()
 </script>
 
 <div>
 	<div class="text-slate-700 pb-2 dark:text-white">{title}</div>
 	{#if !postfix}
 		<div>
-			<input value={value} {...{type}} class="input" onchange={({target}) => onchange(target.value)} />
+			<input value={value} {...{type}} class="input" onchange={({target}) => onchange(target.value)} onkeyup={({target}) => onkeyup(target.value)} />
 		</div>
 	{/if}
 	{#if postfix}
 		<div class="combo">
-			<input value={value} {...{type}} class="input" onchange={({target}) => onchange(target.value)} />
+			<input value={value} {...{type}} class="input" onchange={({target}) => onchange(target.value)} onkeyup={({target}) => onkeyup(target.value)} />
 			<span>{postfix}</span>
 		</div>
 	{/if}
