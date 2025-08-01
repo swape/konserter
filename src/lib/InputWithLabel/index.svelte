e4a7480f90523978149b31da9624c815ce401853
Alireza Balouch <gswape@gmail.com>
2025-08-01 14:18:05 +0200

    style: adjust input max-width for better layout and improve structure in band search page

--- src/lib/InputWithLabel/index.svelte
+++ src/lib/InputWithLabel/index.svelte
@@ -6,7 +6,7 @@ let id = getUniqueId(type)
 </script>
 
 <label for={id}>
-	<div class="text-slate-700 pb-2 dark:text-white">{title}</div>
+	<div class="pb-2 text-black dark:text-white">{title}</div>
 	{#if !postfix}
 		<div>
 			<input id={id} value={value} {...{type}} class="input" onchange={({target}) => onchange(target.value)} onkeyup={({target}) => onkeyup(target.value)} />
--- src/pages/bandSearch/index.svelte
+++ src/pages/bandSearch/index.svelte
@@ -7,8 +7,10 @@ import {searchedBandName} from '../../myStore.ts'
 <main>
 	<div class="container mx-auto p-3">
 		<h1 class="text-2xl text-center mb-2 text-white">Søk etter band</h1>
-		<p class="text-center mb-3 text-white">Skriv inn navnet på bandet du leter etter:</p>
-		<InputWithLabel title="Bandnavn" value={$searchedBandName} onkeyup={(value) => ($searchedBandName = value)} />
+		<p class="text-center mb-3 text-white text-sm">Skriv inn navnet på bandet du leter etter:</p>
+		<div class="p-3 bg-gray-100 rounded-md shadow-md dark:bg-gray-900">
+			<InputWithLabel title="Bandnavn" value={$searchedBandName} onkeyup={(value) => ($searchedBandName = value)} />
+		</div>
 	</div>
 	<ConcertList limit={null} artist={$searchedBandName} />
 </main>
