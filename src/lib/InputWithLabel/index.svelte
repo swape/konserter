0ef64ee488fbac8a18e1a6b8e025aac1bafca5bc
Alireza Balouch <gswape@gmail.com>
2025-08-01 14:05:22 +0200

    fix: wrap input fields in label for improved accessibility

--- src/lib/InputWithLabel/index.svelte
+++ src/lib/InputWithLabel/index.svelte
@@ -1,18 +1,21 @@
 <script>
+import {getUniqueId} from '../../helper.ts'
+
 let {value = null, title = '', type = 'text', postfix = null, onchange = () => {}, onkeyup = () => {}} = $props()
+let id = getUniqueId(type)
 </script>
 
-<div>
+<label for={id}>
 	<div class="text-slate-700 pb-2 dark:text-white">{title}</div>
 	{#if !postfix}
 		<div>
-			<input value={value} {...{type}} class="input" onchange={({target}) => onchange(target.value)} onkeyup={({target}) => onkeyup(target.value)} />
+			<input id={id} value={value} {...{type}} class="input" onchange={({target}) => onchange(target.value)} onkeyup={({target}) => onkeyup(target.value)} />
 		</div>
 	{/if}
 	{#if postfix}
 		<div class="combo">
-			<input value={value} {...{type}} class="input" onchange={({target}) => onchange(target.value)} onkeyup={({target}) => onkeyup(target.value)} />
+			<input id={id} value={value} {...{type}} class="input" onchange={({target}) => onchange(target.value)} onkeyup={({target}) => onkeyup(target.value)} />
 			<span>{postfix}</span>
 		</div>
 	{/if}
-</div>
+</label>
