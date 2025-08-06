99205359b50bc7673679411e0b51aa2daceed485
Alireza Balouch <gswape@gmail.com>
2025-08-06 18:30:48 +0200

    refactor: reorganize authentication logic for improved clarity

--- src/lib/AuthGate/AuthGate.svelte
+++ src/lib/AuthGate/AuthGate.svelte
@@ -1,17 +1,19 @@
 <script>
 import {isAuthenticated, signIn} from '../../myStore.ts'
 import Header from '../Header/Header.svelte'
-
+import {initTheAuth} from '../../fire.js'
 import InputWithLabel from '../InputWithLabel/index.svelte'
+
+initTheAuth()
 </script>
 
-{#if $isAuthenticated}
-	<Header />
-	<slot />
-{/if}
 {#if !$isAuthenticated}
 	<main class="flex flex-col items-center">
 		<img src="/konserter-96.png" alt="konserter logo" class="logo" />
 		<button class="button mt-5" onclick={signIn}>Logg inn med Google</button>
 	</main>
+{/if}
+{#if $isAuthenticated}
+	<Header />
+	<slot />
 {/if}
