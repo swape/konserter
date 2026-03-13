2bb566ddee923887e639834feee22dd76d480c20
Alireza Balouch <gswape@gmail.com>
2026-03-13 09:05:48 +0100

    feat(bandInfoBox): 🎸 enhance artist search functionality and improve UI for displaying results

--- src/lib/BandInfoBox/index.svelte
+++ src/lib/BandInfoBox/index.svelte
@@ -1,18 +1,103 @@
 <script>
-import {searchArtistFromFirebaseByMBID} from '../../musicBrainz.js'
-const {mbid} = $props()
-let bandInfo = $state(null)
+import {searchArtistFromFirebaseByMBID, searchArtistFromMusicBrainz, convertToBandInfo, addArtistInfoToFirebase} from '../../musicBrainz.js'
 
-searchArtistFromFirebaseByMBID(mbid, (data) => {
-	if (data) {
-		bandInfo = data
-	}
+let {mbid = $bindable(), artistName = $bindable(), updateBandInfo} = $props()
+let bandInfo = $state(null)
+let showAllArtists = $state(false)
+let searchedResults = $state([])
+
+$effect(() => {
+	searchArtistFromFirebaseByMBID(mbid, (data) => {
+		if (data) {
+			bandInfo = data
+		}
+	})
 })
+
+function searchAndShowArtists(artistName) {
+	searchArtistFromMusicBrainz(artistName, true).then((data) => {
+		if (data?.length > 0) {
+			searchedResults = data.map(convertToBandInfo)
+			// console.log('searchedResults', data)
+			showAllArtists = true
+		}
+	})
+}
+
+function selectArtist(item) {
+	mbid = item.mbid
+	bandInfo = item
+	showAllArtists = false
+	addArtistInfoToFirebase(mbid, item)
+	updateBandInfo(item)
+}
+
+function confirmThis() {
+	const confirmed = confirm('Er du sikker på at du vil fjerne artistinfo? Dette vil ikke slette konserten')
+	if (confirmed) {
+		bandInfo = null
+		updateBandInfo(null)
+	}
+}
 </script>
 
-<div>
+<div class="mb-2 relative">
+	{#if bandInfo}
+		<span class="absolute right-0">
+			<button onclick={confirmThis}><span class="material-icons">delete</span></button>
+		</span>
+	{/if}
 	{#if bandInfo?.data?.country}
 		<div>Opprinnelsesland: {bandInfo?.data?.country}</div>{/if}
 	{#if bandInfo?.data?.genre}
-		<div>Sjanger: {bandInfo?.data?.genre}</div>{/if}
+		<div>
+			Sjanger: {#if bandInfo.data.disambiguation}
+				<span class="italic text-sm">({bandInfo.data.disambiguation})</span>
+			{/if}
+			<span>{bandInfo?.data?.genre}</span>
+		</div>
+	{/if}
+
+	{#if !bandInfo}
+		<div class="mb-3">
+			<button class="button gray" onclick={() => searchAndShowArtists(artistName)}> Hent info om {artistName} </button>
+		</div>
+
+		<div>
+			{#if showAllArtists}
+				<div class="mt-2">
+					<div class="font-bold">Flere artister med samme navn:</div>
+					{#each searchedResults as item}
+						<div class="mt-1 box">
+							<div>
+								<div>
+									{item.artist}
+									{#if item.data.disambiguation}
+										<span class="italic text-sm">({item.data.disambiguation})</span>
+									{/if}
+								</div>
+								<div>
+									{#if item.data.type}
+										<div>{item.data.type}</div>
+									{/if}
+									{#if item.data.country}
+										<div>Land: {item.data.country}</div>
+									{/if}
+									{#if item.data.genre}
+										<div>Sjanger: {item.data.genre}</div>
+									{/if}
+								</div>
+							</div>
+							<div>
+								<button class="button small" onclick={() => selectArtist(item)}>Velg denne</button>
+							</div>
+						</div>
+					{/each}
+				</div>
+				<div class="mt-2">
+					<button class="button small gray" onclick={() => (showAllArtists = false)}>Lukk</button>
+				</div>
+			{/if}
+		</div>
+	{/if}
 </div>
