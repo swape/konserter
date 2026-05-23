a4cf6cad3222482a0e9d802d634585e9c900f2e8
Alireza Balouch <gswape@gmail.com>
2026-05-23 16:38:42 +0200

    feat(api): ✨ implement GET endpoint to fetch fanart by musicBrainzId

--- api/fanart.ts
+++ api/fanart.ts
@@ -1,9 +1,15 @@
-export function GET(request: Request) {
+import {fetchFanart} from '../src/fanart'
+
+export async function GET(request: Request) {
 	const musicBrainzId = new URL(request.url).searchParams.get('mbid')
 	if (!musicBrainzId) { 
 		return new Response('Missing musicBrainzId', { status: 400 })
 	}
 
+	const fanart = await fetchFanart(musicBrainzId)
+	if (!fanart) {
+		return new Response('Failed to fetch fanart', { status: 500 })
+	}
 
-	return new Response('Hello from Vercel!')
+	return new Response(JSON.stringify(fanart), { status: 200 })
 }
