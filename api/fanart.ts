f3a5f2f0221adfe4545a2b3af674e873ed28d73c
Alireza Balouch <gswape@gmail.com>
2026-05-23 16:41:36 +0200

    feat(api): ✨ add fetchFanart function and update GET endpoint to use fanart API

--- api/fanart.ts
+++ api/fanart.ts
@@ -1,4 +1,5 @@
-import {fetchFanart} from '../src/fanart'
+const API_KEY = import.meta.env.FANART_API_KEY
+const BASE_URL = 'https://webservice.fanart.tv/v3.2/music/'
 
 export async function GET(request: Request) {
 	const musicBrainzId = new URL(request.url).searchParams.get('mbid')
@@ -12,4 +13,24 @@ export async function GET(request: Request) {
 	}
 
 	return new Response(JSON.stringify(fanart), { status: 200 })
+}
+
+
+function getFanartUrl(musicBrainzId: string) {
+	return `${BASE_URL}${musicBrainzId}?api_key=${API_KEY}`
+}
+
+// this lives on server
+export async function fetchFanart(musicBrainzId: string): Promise<string | null> {
+	try {
+		const response = await fetch(getFanartUrl(musicBrainzId))
+		if (!response.ok) {
+			console.error('Failed to fetch fanart:', response.statusText)
+			return null
+		}
+		return await response.json()
+	} catch (error) {
+		console.error('Error fetching fanart:', error)
+		return null
+	}
 }
