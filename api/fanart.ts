60319121aaf30ea3a683f755ccd1c0f48d51aa47
Alireza Balouch <gswape@gmail.com>
2026-05-23 16:35:08 +0200

    fix(api): 🐛 handle missing musicBrainzId in GET request

--- api/fanart.ts
+++ api/fanart.ts
@@ -1,3 +1,9 @@
 export function GET(request: Request) {
+	const musicBrainzId = new URL(request.url).searchParams.get('mbid')
+	if (!musicBrainzId) { 
+		return new Response('Missing musicBrainzId', { status: 400 })
+	}
+
+
 	return new Response('Hello from Vercel!')
 }
